b003e3032bea863a05cc910e91364b6d30f4341c
dorababue <dorababue@ontophi.com>
2026-08-20 17:04:37 +0530

    Delete lib directory
    lib files not using with current website

--- lib/metadata.ts
+++
@@ -1,7 +0,0 @@
-import { Metadata } from "next";
-
-export const metadata: Metadata = {
-  title: "OntoPhi",
-  description:
-    "Advancing intelligent computing through research, engineering, and systems software.",
-};
